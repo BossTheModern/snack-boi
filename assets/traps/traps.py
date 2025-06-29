f636e4ba79bf28845de4261b700afd1b29de11ec
Phrot Vedal <phrotv@stud.ntnu.no>
2025-06-29 13:53:35 +0200

    Fixed a bug where traps spawn without showing itself while recon snack is active

--- assets/traps/traps.py
+++ assets/traps/traps.py
@@ -7,6 +7,8 @@ from typing import List
 import random
 
 class Trap:
+    revealed: bool = False
+
     def spawn_trap(self, board: List[List[str]], occupied_positions: List[List[int]]) -> None:
         '''
             Spawns a trap on the board at a random available position. The
@@ -17,10 +19,14 @@ class Trap:
         self._position = [random.randint(0, len(board)-1), random.randint(0, len(board)-1)]
 
         while self._position in occupied_positions or board[self._position[0]][self._position[1]] != ' ':
-            self._position = [random.randint(0, len(board)-1), random.randint(0, len(board)-1)]    
+            self._position = [random.randint(0, len(board)-1), random.randint(0, len(board)-1)] 
+
+        if self.revealed:
+            board[self._position[0]][self._position[1]] = self._trap_entity   
     
     def reveal_trap(self, board: List[List[str]]) -> None:
         board[self._position[0]][self._position[1]] = self._trap_entity
+        self.revealed = True
     
     def hide(self, board: List[List[str]]) -> None:
         board[self._position[0]][self._position[1]] = ' '
