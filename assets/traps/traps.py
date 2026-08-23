68149c1aac15e9f5a48716964e5baefec1ecd144
BossTheModern <prhotv@stud.ntnu.no>
2026-08-23 16:27:57 +0200

    used spawnable spawn, used _entity attribute

--- assets/traps/traps.py
+++ assets/traps/traps.py
@@ -7,19 +7,18 @@ from typing import List
 from boards.board import Board
 from assets.position.position2d import Position2D
 from assets.spawnable.spawnable import Spawnable
-import random
 
 class Trap(Spawnable):
     revealed: bool = False
     
     def spawn(self, board: Board, occupied_positions: List[Position2D]) -> None:
-        super().spawn(board, occupied_positions)
+        super().spawn(board, occupied_positions, hidden=True)
 
         if self.revealed:
-            board.set(self._position.x, self._position.y, self._trap_entity)
+            board.set(self._position.x, self._position.y, self._entity)
 
     def reveal_trap(self, board: Board) -> None:
-        board.set(self._position.x, self._position.y, self._trap_entity)
+        board.set(self._position.x, self._position.y, self._entity)
         self.revealed = True
     
     def hide(self, board: Board) -> None:
