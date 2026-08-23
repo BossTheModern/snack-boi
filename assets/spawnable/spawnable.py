6ebada2b34a1da7456cebda392b59233d894a124
BossTheModern <prhotv@stud.ntnu.no>
2026-08-23 16:12:23 +0200

    changed spawn function to optionally enable hiding entity

--- assets/spawnable/spawnable.py
+++ assets/spawnable/spawnable.py
@@ -14,22 +14,19 @@ class Spawnable:
         self._position: Position2D = Position2D()
         self._entity: str = entity
 
-    def spawn(self, board: Board, occupied_positions: List[Position2D] = []) -> None:
+    def spawn(self, board: Board, occupied_positions: List[Position2D] = [], hidden: bool = False) -> None:
         '''
-            randomly assigns a position to spawnable
-
-            NOTE: This only assigns a random position to spawnable and does not
-            add the entity explicitly to the board due to traps existing. Explicitly
-            adding the entity to the board requires overriding.
+            randomly assigns a position to spawnable with the option of
+            hiding the entity from the board
         '''
         random_x: int = random.randint(0, board._width-1)
         random_y: int = random.randint(0, board._height-1)
         self._position.set(random_x, random_y)
         
-        while self._position in occupied_positions or board.at(self._position.x, self._position.y) != ' ':
+        while self._position in occupied_positions or not board.is_available_at(self._position):
             random_x = random.randint(0, board._width-1)
             random_y = random.randint(0, board._height-1)
             self._position.set(random_x, random_y)
 
-        
-
+        if not hidden:
+            board.set(self._position.x, self._position.y, self._entity)
