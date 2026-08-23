c65a161d73fb537c4b8a6ba6304e9d1581f16e9b
BossTheModern <prhotv@stud.ntnu.no>
2026-08-23 15:22:39 +0200

    added overridden spawn function

--- assets/traps/traps.py
+++ assets/traps/traps.py
@@ -6,9 +6,10 @@
 from typing import List
 from boards.board import Board
 from assets.position.position2d import Position2D
+from assets.spawnable.spawnable import Spawnable
 import random
 
-class Trap:
+class Trap(Spawnable):
     revealed: bool = False
 
     def __init__(self) -> None:
@@ -32,7 +33,13 @@ class Trap:
 
         if self.revealed:
             board.set(self._position.x, self._position.y, self._trap_entity)
-    
+
+    def spawn(self, board: Board, occupied_positions: List[Position2D]) -> None:
+        super().spawn(board, occupied_positions)
+
+        if self.revealed:
+            board.set(self._position.x, self._position.y, self._trap_entity)
+
     def reveal_trap(self, board: Board) -> None:
         board.set(self._position.x, self._position.y, self._trap_entity)
         self.revealed = True
