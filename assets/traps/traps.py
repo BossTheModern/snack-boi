945f8e260ac385810e5ddadd6d15d243b519bbfd
BossTheModern <prhotv@stud.ntnu.no>
2026-08-23 15:35:04 +0200

    removed init

--- assets/traps/traps.py
+++ assets/traps/traps.py
@@ -11,29 +11,7 @@ import random
 
 class Trap(Spawnable):
     revealed: bool = False
-
-    def __init__(self) -> None:
-        self._position: Position2D = Position2D()
-
-    def spawn_trap(self, board: Board, occupied_positions: List[Position2D]) -> None:
-        '''
-            Spawns a trap on the board at a random available position. The
-            traps are hidden by default
-            Args:
-                board: The board where the trap will be spawned.     
-        '''
-        random_x: int = random.randint(0, board._width-1)
-        random_y: int = random.randint(0, board._height-1)
-        self._position.set(random_x, random_y)
-
-        while self._position in occupied_positions or board.at(self._position.x, self._position.y) != ' ':
-            random_x = random.randint(0, board._width-1)
-            random_y = random.randint(0, board._height-1)
-            self._position.set(random_x, random_y)
-
-        if self.revealed:
-            board.set(self._position.x, self._position.y, self._trap_entity)
-
+    
     def spawn(self, board: Board, occupied_positions: List[Position2D]) -> None:
         super().spawn(board, occupied_positions)
 
