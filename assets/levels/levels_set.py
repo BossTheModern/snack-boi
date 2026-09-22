7aaec39d1243606b1cf36e84189ae12ea6c220d9
BossTheModern <prhotv@stud.ntnu.no>
2026-09-22 12:07:42 +0200

    added 5 new levels to set

--- assets/levels/levels_set.py
+++ assets/levels/levels_set.py
@@ -6,6 +6,7 @@
 
 from assets.levels.levels import Levels
 from assets.levels.level import Level
+from assets.teleport_pod.teleport_pods import TeleportPods
 
 from boards.grid_collection import square_obstacle_grid, square_obstacle_grid_2
 from boards.grid_collection import square_obstacle_grid_3, square_obstacle_grid_4
@@ -17,6 +18,10 @@ from boards.grid_collection import square_obstacle_grid_13, square_obstacle_grid
 from boards.grid_collection import square_obstacle_grid_15, square_obstacle_grid_16
 from boards.grid_collection import square_obstacle_grid_17, square_obstacle_grid_18
 from boards.grid_collection import square_obstacle_grid_19, square_obstacle_grid_20
+from boards.grid_collection import square_obstacle_grid_21, square_obstacle_grid_22
+from boards.grid_collection import square_obstacle_grid_23, square_obstacle_grid_24
+from boards.grid_collection import square_obstacle_grid_25
+
 
 levels_set: Levels = Levels(
     [Level(square_obstacle_grid), 
@@ -38,5 +43,10 @@ levels_set: Levels = Levels(
      Level(square_obstacle_grid_17),
      Level(square_obstacle_grid_18),
      Level(square_obstacle_grid_19),
-     Level(square_obstacle_grid_20),]
+     Level(square_obstacle_grid_20),
+     Level(square_obstacle_grid_21, teleport_pods=TeleportPods()),
+     Level(square_obstacle_grid_22, teleport_pods=TeleportPods()),
+     Level(square_obstacle_grid_23, teleport_pods=TeleportPods()),
+     Level(square_obstacle_grid_24, teleport_pods=TeleportPods()),
+     Level(square_obstacle_grid_25, teleport_pods=TeleportPods()),]
 )
